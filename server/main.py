1a8be65e20da603b193d9f7a6810649178d845f8
fgalvanbaez <francisco.galvan.baez@gmail.com>
2017-06-13 09:22:46 +0100

    Hago las modificaciones en el ejercicio para que se pasen los tests

--- server/main.py
+++ server/main.py
@@ -6,14 +6,41 @@ PUBLISHER_SERVER_PORT = 9999
 SUBSCRIBER_SERVER_HOST = '127.0.0.1'
 SUBSCRIBER_SERVER_PORT = 8888
 
-async def handle_publisher(reader, writer):
+#publishers = []
+subscribers = []
+
+async def handle_publisher(reader, my_writer):
     # Implementar aqui el manejo de sockets de publishers
-    pass
+    #publishers.append(reader)
+    try :
+        while True :
+            message_length_bytes = await reader.read(4)
+            message_length = int.from_bytes(
+            message_length_bytes, byteorder='big')
+            message = await reader.read(message_length)
+            for subscriber in subscribers:
+                #if subscriber != my_writer:
+                subscriber.write(message_length_bytes)
+                subscriber.write(message)
+    except IncompleteReadError:
+        pass
+        #publishers.remove(my_writer)
 
 
-async def handle_subscriber(reader, writer):
-    # Implementar aqui el manejo de sockets de subscriptores
-    pass
+async def handle_subscriber(reader, my_writer):
+    # Implementar aqui el manejo de sockets de suscriptores
+    subscribers.append(my_writer)
+    try :
+        while True :
+            for subscriber in subscribers:
+                #if subscriber != reader:
+                    #asyncio.Protocol.data_received(data)
+                message_length_bytes = await reader.read(4)
+                message_length = int.from_bytes(
+                message_length_bytes, byteorder='big')
+                message = await reader.read(message_length)
+    except IncompleteReadError:
+       subscribers.remove(my_writer)
 
 
 def start_server():
@@ -26,6 +53,10 @@ def start_server():
     )
     subscriber_server = loop.run_until_complete(subscriber_server_fut)
     publisher_server = loop.run_until_complete(publisher_server_fut)
+
+    # Serve requests until Ctrl+C is pressed
+    print('Serving subscribers on {}'.format(subscriber_server.sockets[0].getsockname()))
+    print('Serving publishers on {}'.format(publisher_server.sockets[0].getsockname()))
 
     try:
         loop.run_forever()
